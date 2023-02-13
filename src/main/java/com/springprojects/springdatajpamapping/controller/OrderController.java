72711a3c158e5caf5dad440d05715bef12ae9c8d
Liza <lixia101111@gmail.com>
2023-02-13 16:39:53 +0200

    Add GetMapping and PostMapping for orders

--- src/main/java/com/springprojects/springdatajpamapping/controller/OrderController.java
+++ src/main/java/com/springprojects/springdatajpamapping/controller/OrderController.java
@@ -1,4 +1,29 @@
 package com.springprojects.springdatajpamapping.controller;
 
+import com.springprojects.springdatajpamapping.dto.OrderRequest;
+import com.springprojects.springdatajpamapping.entity.Customer;
+import com.springprojects.springdatajpamapping.repository.CustomerRepository;
+import com.springprojects.springdatajpamapping.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class OrderController {
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
+    private ProductRepository productRepository;
+    @PostMapping("/placeOrder")
+    public Customer placeOrder(@RequestBody OrderRequest request){
+        return customerRepository.save(request.getCustomer());
+    }
+    @GetMapping("/findAllOrders")
+    public List<Customer> findAllOrders(){
+        return customerRepository.findAll();
+    }
 }
